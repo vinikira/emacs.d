a4a6435fb46d76367ccc7037024604f3ba8ad6bb
Vinícius Simões <Vincius Simes>
2017-09-11 15:49:36 -0300

    add monokai theme

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,8 +1,11 @@
-(use-package spacemacs-theme
+;; (use-package spacemacs-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'spacemacs-dark t))
+
+(use-package monokai-theme
   :defer t
   :init
-  (load-theme 'spacemacs-dark t))
-
-;; (use-package monokai-theme)
+  (load-theme 'monokai t))
 
 (provide 'base-theme)
