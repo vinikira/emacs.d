1265a28918c500d08ae4b54c0b708f137da9a41a
Vinícius Simões <Vincius Simes>
2017-10-19 10:38:54 -0200

    add prettier-js and fix some bugs

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -1,11 +1,11 @@
-;; (use-package spacemacs-theme
-;;   :defer t
-;;   :init
-;;   (load-theme 'spacemacs-dark t))
-
-(use-package monokai-theme
+(use-package spacemacs-theme
   :defer t
   :init
-  (load-theme 'monokai t))
+  (load-theme 'spacemacs-dark t))
+
+;; (use-package monokai-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'monokai t))
 
 (provide 'base-theme)
--- elisp/lang-javascript.el
+++ elisp/lang-javascript.el
@@ -1,6 +1,7 @@
 ;; js2-mode
 ;; https://github.com/mooz/js2-mode
 (use-package js2-mode
+  :diminish Javascript-IDE "JS"
   :bind (:map js2-mode-map
               (("C-x C-e" . js-send-last-sexp)
                ("C-M-x" . js-send-last-sexp-and-go)
@@ -8,47 +9,56 @@
                ("C-c C-l" . js-load-file-and-go)))
   :mode
   ("\\.js$" . js2-mode)
+  ("\\.jsx$" . js2-jsx-mode)
   :config
   (custom-set-variables '(js2-strict-inconsistent-return-warning nil))
-  (custom-set-variables '(js2-strict-missing-semi-warning nil))
+  (custom-set-variables '(js2-strict-missing-semi-warning nil)))
 
-  (use-package json-mode
-    :mode
-      ("\\.json$" . json-mode))
+(use-package json-mode
+  :mode
+  ("\\.json$" . json-mode))
 
-  (setq js-indent-level 4)
-  (setq js2-indent-level 4)
-  (setq js2-basic-offset 4)
+(setq js-indent-level 4)
+(setq js2-indent-level 4)
+(setq js2-basic-offset 4)
 
-  ;; tern :- IDE like features for javascript and completion
-  ;; http://ternjs.net/doc/manual.html#emacs
-  (use-package tern
-    :config
-    (defun my-js-mode-hook ()
-      "Hook for `js-mode'."
-      (set (make-local-variable 'company-backends)
-           '((company-tern company-files))))
-    (add-hook 'js2-mode-hook 'my-js-mode-hook)
-    (add-hook 'js2-mode-hook 'company-mode))
+;; tern :- IDE like features for javascript and completion
+;; http://ternjs.net/doc/manual.html#emacs
+(use-package tern
+  :diminish tern-mode "Tern"
+  :config
+  (defun my-js-mode-hook ()
+    "Hook for `js-mode'."
+    (set (make-local-variable 'company-backends)
+	 '((company-tern company-files))))
+  (add-hook 'js2-mode-hook 'my-js-mode-hook)
+  (add-hook 'js2-mode-hook 'company-mode)
+  (add-hook 'js2-mode-hook 'tern-mode))
 
-  (add-hook 'js2-mode-hook 'tern-mode)
+;; company backend for tern
+;; http://ternjs.net/doc/manual.html#emacs
+(use-package company-tern)
 
-  ;; company backend for tern
-  ;; http://ternjs.net/doc/manual.html#emacs
-  (use-package company-tern)
+;; Run a JavaScript interpreter in an inferior process window
+;; https://github.com/redguardtoo/js-comint
+(use-package js-comint
+  :config
+  (setq inferior-js-program-command "node"))
 
-  ;; Run a JavaScript interpreter in an inferior process window
-  ;; https://github.com/redguardtoo/js-comint
-  (use-package js-comint
-    :config
-    (setq inferior-js-program-command "node"))
-
-  ;; js2-refactor :- refactoring options for emacs
-  ;; https://github.com/magnars/js2-refactor.el
-  (use-package js2-refactor :defer t
-    :diminish js2-refactor-mode
-    :config
-    (js2r-add-keybindings-with-prefix "C-c j r"))
+;; js2-refactor :- refactoring options for emacs
+;; https://github.com/magnars/js2-refactor.el
+(use-package js2-refactor :defer t
+  :diminish js2-refactor-mode "JSR"
+  :config
+  (js2r-add-keybindings-with-prefix "C-c j r")
   (add-hook 'js2-mode-hook 'js2-refactor-mode))
+
+;; prettier-js - format javascript source codes
+;; https://github.com/prettier/prettier-emacs
+(use-package prettier-js
+  :diminish
+  :config
+  (add-hook 'js2-mode-hook 'prettier-js-mode)
+  (setq prettier-js-args '("--tab-width" "4")))
 
 (provide 'lang-javascript)
