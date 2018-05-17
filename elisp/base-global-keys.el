62cbed465d1549aebc00e523840dd113f717c4ae
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-05-17 14:25:53 -0300

    add hotkeys to resize buffers

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -7,5 +7,10 @@
 (global-set-key (kbd "C-x 2") 'split-window-below-and-switch)
 (global-set-key (kbd "C-x 3") 'split-window-right-and-switch)
 
+(global-set-key (kbd "M-<down>") 'enlarge-window)
+(global-set-key (kbd "M-<up>") 'shrink-window)
+(global-set-key (kbd "M-<left>") 'enlarge-window-horizontally)
+(global-set-key (kbd "M-<right>") 'shrink-window-horizontally)
+
 (provide 'base-global-keys)
 ;;; base-global-keys ends here
