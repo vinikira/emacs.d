9d479a1021767ad115410703b3ad26fd46e49707
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-06-10 17:51:18 -0300

    Add clojure-mode, cider and up use-package elxir-mode

---
+++ elisp/lang-clojure.el
@@ -0,0 +1,9 @@
+;;; lang-clojure.el --- Clojure lang
+;;; Commentary:
+;;; Code:
+(use-package clojure-mode)
+
+(use-package cider)
+
+(provide 'lang-clojure)
+;;; lang-clojure ends here
--- elisp/lang-elixir.el
+++ elisp/lang-elixir.el
@@ -1,14 +1,14 @@
 ;;; lang-elixir.el --- Elixir Lang
 ;;; Commentary:
 ;;; Code:
+(use-package elixir-mode)
+
 (use-package alchemist
   :config
   (add-hook 'elixir-mode-hook 'alchemist-mode))
 
 (use-package flycheck-mix
   :commands (flycheck-mix-setup))
-
-(use-package elixir-mode)
 
 (provide 'lang-elixir)
 ;;; lang-elixir ends here
