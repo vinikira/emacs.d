77d2ae16badc97dc558b9bd3b53dc2dfe1dc92b0
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-06-10 13:39:31 -0300

    add emacs bootstrap elixir packages

--- elisp/lang-elixir.el
+++ elisp/lang-elixir.el
@@ -1,7 +1,14 @@
 ;;; lang-elixir.el --- Elixir Lang
 ;;; Commentary:
 ;;; Code:
-(use-package alchemist)
+(use-package alchemist
+  :config
+  (add-hook 'elixir-mode-hook 'alchemist-mode))
+
+(use-package flycheck-mix
+  :commands (flycheck-mix-setup))
+
+(use-package elixir-mode)
 
 (provide 'lang-elixir)
 ;;; lang-elixir ends here
