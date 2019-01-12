11922d563df22ee63257353eb821c16d3b96fc9d
Vinícius Simões <vinicius.simoes95@gmail.com>
2019-01-12 18:03:05 -0200

    refacotr python mode

--- elisp/lang-python.el
+++ elisp/lang-python.el
@@ -1,59 +1,35 @@
 ;;; package --- Python Lang
 ;;; Commentary:
 ;;; Contains my python configs
+;;; packages to install before use: pip install flake8 jedi autopep8 rope yapf ipython
+;;; archlinux: yay -S autopep8 flake8 python-jedi python-rope yapf ipython
 ;;; Code:
 
 (use-package python
   :mode ("\\.py" . python-mode)
-  :config
-  (use-package elpy
-    :init
-    (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-    :config
-    (setq elpy-rpc-backend "jedi")
-    ;; (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-    ;;flycheck-python-flake8-executable "/usr/local/bin/flake8"
-    :bind (:map elpy-mode-map
+  :config (setq python-shell-interpreter "ipython"
+		python-shell-interpreter-args "-i --simple-prompt"))
+
+(use-package elpy
+  :hook ((python-mode . elpy-mode)
+	 (python-mode . elpy-enable))
+  :custom
+  (elpy-rpc-backend "jedi")
+  :bind (:map elpy-mode-map
 	      ("M-." . elpy-goto-definition)
-	      ("M-," . pop-tag-mark)))
-  (elpy-enable))
+	      ("M-," . pop-tag-mark)
+	      ("<M-left>" . nil)
+	      ("<M-right>" . nil)
+	      ("<M-S-left>" . elpy-nav-indent-shift-left)
+	      ("<M-S-right>" . elpy-nav-indent-shift-right)
+	      ("C-c i" . elpy-autopep8-fix-code)
+	      ("C-c C-d" . elpy-doc)))
 
 (use-package pip-requirements
-  :config
-  (add-hook 'pip-requirements-mode-hook #'pip-requirements-auto-complete-setup))
+  :hook ((pip-requirements-mode . #'pip-requirements-auto-complete-setup )))
 
-(use-package py-autopep8)
-
-(use-package pyenv-mode
-  :init
-  (add-to-list 'exec-path "~/.pyenv/shims")
-  (setenv "WORKON_HOME" "~/.pyenv/versions/")
-  :config
-  (add-hook 'projectile-after-switch-project-hook 'pyenv-activate-current-project)
-  (add-hook 'python-mode 'pyenv-init)
-  (add-hook 'python-mode 'pyenv-mode)
-  :bind
-  ("C-x p e" . pyenv-activate-current-project))
-
-(defun pyenv-init()
-  (setq global-pyenv (replace-regexp-in-string "\n" "" (shell-command-to-string "pyenv global")))
-  (message (concat "Setting pyenv version to " global-pyenv))
-  (pyenv-mode-set global-pyenv)
-  (defvar pyenv-current-version nil global-pyenv))
-
-(defun pyenv-activate-current-project ()
-  "Automatically activates pyenv version if .python-version file exists."
-  (interactive)
-  (f-traverse-upwards
-   (lambda (path)
-     (message path)
-     (let ((pyenv-version-path (f-expand ".python-version" path)))
-       (if (f-exists? pyenv-version-path)
-           (progn
-             (setq pyenv-current-version (s-trim (f-read-text pyenv-version-path 'utf-8)))
-             (pyenv-mode-set pyenv-current-version)
-             (pyvenv-workon pyenv-current-version)
-             (message (concat "Setting virtualenv to " pyenv-current-version))))))))
+(use-package py-autopep8
+  :hook ((python-mode . py-autopep8-enable-on-save)))
 
 (provide 'lang-python)
 ;;; lang-python.el ends here
