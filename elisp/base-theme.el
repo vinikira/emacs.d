1b3b694cecdaa89b944be6c2301eee11f553fd2c
Vinícius Simões <vinicius.simoes95@gmail.com>
2019-04-30 12:10:19 -0300

    add check to verify if theme was loaded.

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -5,18 +5,20 @@
 (defvar vs/chosen-theme 'doom-dracula)
 (defvar vs/chosen-font-name "xos4 Terminus")
 (defvar vs/chosen-font-size 12)
+(defvar vs/theme-loaded nil)
 
 (defun vs/load-my-theme-config (&optional frame)
   "Load config for current FRAME."
   (when frame (select-frame frame))
-  (load-theme vs/chosen-theme t)
+  (unless vs/theme-loaded (load-theme vs/chosen-theme t))
   (when (member vs/chosen-font-name (font-family-list))
     (add-to-list 'default-frame-alist `(font . ,(format "%s-%d" vs/chosen-font-name vs/chosen-font-size)))
     (set-face-attribute 'default nil :font (format "%s-%d" vs/chosen-font-name vs/chosen-font-size))
-    (set-frame-font (format "%s-%d" vs/chosen-font-name vs/chosen-font-size) nil t)))
+    (set-frame-font (format "%s-%d" vs/chosen-font-name vs/chosen-font-size) nil t))
+  (setq vs/theme-loaded t))
 
 (use-package doom-themes
-  :ensure t
+  :ensure
   :init (setq doom-themes-enable-bold t
               doom-themes-enable-italic t)
   :config (doom-themes-visual-bell-config)
