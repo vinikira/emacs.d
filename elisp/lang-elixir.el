2b8bb67c5430f46b6c002a6f807f1849fdbb04cc
Vinícius Simões <vinicius.simoes95@gmail.com>
2019-05-08 00:34:39 -0300

    new snippet and fix elixir mode

--- elisp/lang-elixir.el
+++ elisp/lang-elixir.el
@@ -7,15 +7,15 @@
   (add-hook 'before-save-hook 'elixir-format nil t))
 
 (use-package elixir-mode
-  :hook (elixir-mode . format-elixir-buffer)
+  :hook ((elixir-mode . format-elixir-buffer)
+	 (elixir-mode . flycheck-mix-setup))
   :mode (("\\.ex$" . elixir-mode)
 	 ("\\.exs$" . elixir-mode)))
 
 (use-package alchemist
   :hook (elixir-mode . alchemist-mode))
 
-(use-package flycheck-mix
-  :commands (flycheck-mix-setup))
+(use-package flycheck-mix)
 
 (provide 'lang-elixir)
 ;;; lang-elixir ends here
